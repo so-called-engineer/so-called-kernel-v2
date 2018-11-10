fa17e1633e679355e937a14b3a11900a5601e246
so-called-engineer <chsdteja@gmail.com>
2018-11-10 15:28:02 +0530

    modified init

--- init/version.c
+++ init/version.c
@@ -47,4 +47,5 @@ const char linux_banner[] =
 const char linux_proc_banner[] =
 	"%s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+    " (so-called-engineer)"
 	" (" LINUX_COMPILER ") %s\n";
